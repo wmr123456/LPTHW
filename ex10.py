b68dd0a199d3499501251a48eca84169086507fd
wangmaorui <844261654@qq.com>
2017-03-23 15:55:18 +0800

    print

---
+++ ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm spllit\non a line."
+backslash_cat = "I'm \\a \\ cat."
+
+fat_cat = """
+	I'll do a list:
+	\t* Cat foot
+	\t* Fishies
+	\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
