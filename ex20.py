c8041d66d4e39a4d4b0caa4325b0d8ac0024c403
wangmaorui <844261654@qq.com>
2017-03-24 14:45:32 +0800

    function and file

--- ex20.py
+++ ex20.py
@@ -22,10 +22,12 @@ print_all(current_file)
 
 print "Now let's rewind, kind of like a tape."
 
+#确保文件从开头位置开始读取
 rewind(current_file)
 
 print "Let's print three linee:"
 
+#/调用print_a_line函数输出行号和内容重复三遍
 current_line = 1
 print_a_line(current_line, current_file)
 
